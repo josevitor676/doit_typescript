9b1fae9673e746766a33b8033e7285335fe9ea0b
José Vitor da Silva Santos <jvss1036@gmail.com>
2022-08-02 15:52:22 -0300

    feature: page login and component Input

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,49 @@
+import { 
+    FormControl, 
+    FormErrorMessage, 
+    FormLabel, 
+    Input as ChakraInput, 
+    InputProps as ChakraInputProps, 
+    InputLeftElement, 
+    InputGroup 
+} from "@chakra-ui/react";
+
+
+import { useState, useEffect, useCallback, useRef } from "react";
+import { FieldError } from "react-hook-form";
+import { IconType } from "react-icons/lib";
+
+
+interface InputProps extends ChakraInputProps {
+    name: string;
+    label?: string;
+    error?: FieldError | null;
+    icon?: IconType;
+}
+
+export const Input = ({name, error = null, icon: Icon, label, ...rest}: InputProps) => {
+    return (
+        <FormControl>
+            {!!label && <FormLabel>{label}</FormLabel> }
+
+            <InputGroup flexDirection="column">
+                {Icon && 
+                    <InputLeftElement mt="2.2">
+                        <Icon/>
+                    </InputLeftElement>
+                }      
+                <ChakraInput 
+                name={name} 
+                bg="gray.50" 
+                variant="outline" 
+                _hover={{bgColor: "gray.100"}}
+                _placeholder={{color: "gray.300"}}
+                size="lg"
+                h="45px"
+                {...rest}/>
+
+                {!!error && <FormErrorMessage>Erro</FormErrorMessage>}
+            </InputGroup>
+        </FormControl>
+    )
+}
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,50 @@
-import { Heading } from "@chakra-ui/react"
+import { Flex, Grid, Heading, Image, Text, VStack } from "@chakra-ui/react"
+import { useForm } from "react-hook-form"
+import LogoSecondary from "../../assets/logo-secondary.svg"
+import { Input } from "../../components/Form/Input"
+import {FaEnvelope, FaLock} from "react-icons/fa"
 
 export const Login = () => {
+
+    const {
+        formState: {errors}, 
+        register,
+        handleSubmit
+    } = useForm()
     return (
-        <Heading>
-            Login
-        </Heading>
+        <Flex 
+        p="10px 15px" 
+        alignItems="center" 
+        h="100vh"
+        bgGradient="linear(to-r, purple.800 65%, white 35%)"
+        color="white"
+        >
+            <Flex w="100%" justifyContent="center" flexDirection="row" alignItems="center">
+                <Grid w="100%" paddingRight="100px">
+                    <Image src={LogoSecondary}/>
+                    <Heading as="h1">
+                        O jeito fácil, grátis
+                    </Heading>
+                    <Text>
+                        Flexível e atrativo de gerenciar
+                        <b>seus projetos em uma única plataforma</b>
+                    </Text>
+                </Grid>
+                <Grid 
+                as="form" 
+                mt="4" 
+                w="100%" 
+                p="30px 15px" 
+                border="3px solid" 
+                borderColor="gray.100" bg="white" color="gray.900"
+                >  
+                    <Heading>Bem vindo de volta!</Heading>
+                    <VStack mt="6" spacing="5">
+                        <Input  icon={FaEnvelope} placeholder="Digite seu login" {...register("email")}/>
+                        <Input icon={FaLock} placeholder="Digite sua senha" {...register("password")}/>
+                    </VStack>
+                </Grid>
+            </Flex>
+        </Flex>
     )
 }
